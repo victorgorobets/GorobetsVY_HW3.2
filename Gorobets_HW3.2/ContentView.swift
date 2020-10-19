0231c1034a3de68ba14e23019c26b44bd7ac4214
VGO <vgorobets@yandex.ru>
2020-10-19 19:10:50 +0300

    Initial Commit

---
+++ Gorobets_HW3.2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gorobets_HW3.2
+//
+//  Created by VICTOR on 19.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gorobets_HW3.2/Gorobets_HW3_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Gorobets_HW3_2App.swift
+//  Gorobets_HW3.2
+//
+//  Created by VICTOR on 19.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct Gorobets_HW3_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
